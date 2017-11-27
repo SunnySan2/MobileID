f3b3ed1ededdafd1b5f07f584f886f968ac66e5a
SunnySan <sunny1227@pchome.com.tw>
2017-11-27 18:41:22 +0800

    2

--- app/src/main/java/com/taisys/sc/mobileid/MainActivity.java
+++ app/src/main/java/com/taisys/sc/mobileid/MainActivity.java
@@ -2,19 +2,32 @@ package com.taisys.sc.mobileid;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class MainActivity extends Activity {
+
+    //CarouselView圖片輪播，參考 https://github.com/sayyam/carouselview
+    CarouselView carouselView;
+    //int[] sampleImages = {R.drawable.banner_tw_01, R.drawable.banner_tw_02, R.drawable.banner_tw_03, R.drawable.banner_tw_04, R.drawable.banner_tw_05, R.drawable.banner_tw_06, R.drawable.banner_tw_07, R.drawable.banner_tw_08, R.drawable.banner_tw_09, R.drawable.banner_tw_10, R.drawable.banner_tw_11};
+    int[] sampleImages = {R.drawable.banner_tw_12, R.drawable.banner_tw_13, R.drawable.banner_tw_14, R.drawable.banner_tw_15};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setCarouselView();
+
         /*
                 Intent intent = getIntent();
                 String msg = intent.getStringExtra("msg");
@@ -58,4 +71,41 @@ public class MainActivity extends Activity {
                 .setIcon(item.getIcon())
                 .show();
     }
+
+    //設定輪播 banner
+    private void setCarouselView(){
+        carouselView = (CarouselView) findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
+        ViewGroup.LayoutParams para = carouselView.getLayoutParams();
+        // 取得螢幕解析度
+        DisplayMetrics dm = new DisplayMetrics();
+        this.getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int i = dm.widthPixels;
+        //Log.d("FCM", "i=" + String.valueOf(i));
+        //i = (108*i/730)-32; //原始檔案是730*108的，減32是因為Layout有paddingLeft及paddingRight各16
+        //i = (i/2)-32; //原始檔案是730*108的，減32是因為Layout有paddingLeft及paddingRight各16
+        i = i/2;
+        //Log.d("FCM", "i=" + String.valueOf(i));
+        para.height = i;
+        carouselView.setLayoutParams(para);
+
+        i=dm.heightPixels;
+        Log.d("FCM", "i=" + String.valueOf(i));
+        i=(i-para.height-32)/2;
+        Log.d("FCM", "i=" + String.valueOf(i));
+        //para.height = i;
+        Button btn = (Button) findViewById(R.id.iconRegistration);
+        //btn.setHeight(i);
+        //btn.setLayoutParams(para);
+    }   //private void setCarouselView(){
+
+    //給輪播 banner用的
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
+
 }
