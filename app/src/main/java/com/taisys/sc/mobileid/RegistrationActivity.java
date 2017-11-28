3a7f6ee87b7d7af501e7bf4eae8b1dd897b8b1fe
SunnySan <sunny1227@pchome.com.tw>
2017-11-28 23:44:03 +0800

    1

---
+++ app/src/main/java/com/taisys/sc/mobileid/RegistrationActivity.java
@@ -0,0 +1,13 @@
+package com.taisys.sc.mobileid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegistrationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
