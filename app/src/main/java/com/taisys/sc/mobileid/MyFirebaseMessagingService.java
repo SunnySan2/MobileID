383ff2cce8fdeff6ea116c3307ecf4cc4c1fe75b
SunnySan <sunny1227@pchome.com.tw>
2017-11-30 18:52:29 +0800

    2

--- app/src/main/java/com/taisys/sc/mobileid/MyFirebaseMessagingService.java
+++ app/src/main/java/com/taisys/sc/mobileid/MyFirebaseMessagingService.java
@@ -17,24 +17,24 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.d("FCM", "onMessageReceived:"+remoteMessage.getFrom());
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
         Map<String, String> messageData = remoteMessage.getData();
         if (messageData!=null && messageData.size()>0){
             for(Map.Entry<String, String> entry : messageData.entrySet()) {
                 String keyName = entry.getKey();
                 String valueString = entry.getValue();
                 Log.d("FCM", "Message data, key=" + keyName + ", value=" + valueString);
+                intent.putExtra(keyName, valueString);
             }
+            ComponentName cn = new ComponentName(this, AuthenticationActivity.class);
+            intent.setComponent(cn);
+            startActivity(intent);
         }else{
             Log.d("FCM", "No Message data");
         }
-
-        Intent intent = new Intent();
-        intent.setAction(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        ComponentName cn = new ComponentName(this, MainActivity.class);
-        intent.setComponent(cn);
-        startActivity(intent);
-
     }
 }
